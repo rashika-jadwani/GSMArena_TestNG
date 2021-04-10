07e8b0692f5fa17f4f98385029c86fa7532578af
Rahul Jadhwani <rahuljadhwani1913@gmail.com>
2021-04-11 01:12:18 +0530

    Added tests for MainMenu
    Added one test for checking count of mainmenu items, class and test setup & closure

--- src/test/java/module_mainmenu/MainMenuTest.java
+++ src/test/java/module_mainmenu/MainMenuTest.java
@@ -1,23 +1,68 @@
 package module_mainmenu;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import ui_pages.HomePage;
 
 public class MainMenuTest {
-    HomePage objHomePage;
+    private HomePage objHomePage;
+    private WebDriver driver;
 
 
     @BeforeClass
-    public void setupClass(){
-        objHomePage = new HomePage();
+    @Parameters({"browser"})
+    public void setupClass(String browserName){
+        if(browserName.toLowerCase().equals("chrome")){
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if( browserName.toLowerCase().equals("firefox")){
+            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver.exe");
+            driver = new FirefoxDriver();
+        }
+        objHomePage = new HomePage(driver);
+    }
+
+    @BeforeMethod
+    public void testSetup(){
+        driver.get("https://www.gsmarena.com/");
+    }
+
+    @Test
+    public void goToNewsPage(){
+
+    }
+
+    @Test
+    public void goToReviewsPage(){
+
+    }
+
+    @Test
+    public void goToVideosPage(){
+
+    }
+
+    @Test
+    public void goToPhoneFinderPage(){
 
     }
 
 
     @Test
     public void mainMenuContents(){
-        Assert.assertEquals(objHomePage.checkMainMenuSize(),8);
+        Assert.assertEquals(objHomePage.checkMainMenuSize(),10);
+    }
+
+    @AfterMethod
+    public void testCleanup(){
+        driver.quit();
+    }
+
+
+    @AfterClass
+    public void closedown(){
     }
 }
